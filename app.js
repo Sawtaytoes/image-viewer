d656bfdac062c474d30bb227501639349092a095
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-28 02:47:25 -0500

    Setup super simple app

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+	const browserWindow = (
+		new BrowserWindow({
+			webPreferences: {
+				nodeIntegration: true
+			}
+		})
+	)
+
+	browserWindow.loadFile('index.html')
+	browserWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
+})
