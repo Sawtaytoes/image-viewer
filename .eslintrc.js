0e1833be11c7a13ac9494c0e5b75cb82cecad296
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-29 02:06:14 -0500

    Removed ESLint requirement for React in scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
 		],
 		'indent': 'off', // TEMP. Remove 'off' when fixed in @ghadyani-eslint
 		'no-unexpected-multiline': 'off',
+		'react/react-in-jsx-scope': 'off',
 		'react-hooks/exhaustive-deps': 'warn',
 		'react-hooks/rules-of-hooks': 'error',
 	},
