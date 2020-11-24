7912bee1624067158bc1f52357e1e3a654ba25b2
Kevin Ghadyani <Saturn2888@gmail.com>
2020-11-24 04:22:23 -0600

    Added thumbnail to Directory
    Thumbnails aren't uniform size and are sometimes wider than the directory container.

--- src/components/fileBrowser/Directory.js
+++ src/components/fileBrowser/Directory.js
@@ -1,12 +1,25 @@
-import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
+import PropTypes from 'prop-types'
 import {
 	memo,
 	useCallback,
 	useContext,
+	useEffect,
+	useState,
 } from 'react'
+import { bindNodeCallback } from 'rxjs'
+import {
+	map,
+	mergeAll,
+	toArray,
+} from 'rxjs/operators'
 
 import FileSystemContext from './FileSystemContext'
+import Image from '../imageViewer/Image'
+import useImageFiles from './useImageFiles'
+
+const fs = global.require('fs')
+const path = global.require('path')
 
 const directoryStyles = css`
 	background-color: #fffffb;
@@ -19,15 +32,27 @@ const directoryStyles = css`
 	width: 100%;
 `
 
-const textStyles = css`
+const directoryContentStyles = css`
 	bottom: 0;
+	display: flex;
+	flex-direction: column;
 	left: 0;
 	padding: 10px 14px;
 	position: absolute;
 	right: 0;
 	top: 0;
+`
+
+const imageStyles = css`
+	flex: 1 1 auto;
+`
+
+const textStyles = css`
+	padding-bottom: 10px;
 	word-wrap: break-word;
 `
+
+const initialDirectoryContents = []
 
 const propTypes = {
 	directoryName: PropTypes.string.isRequired,
@@ -46,6 +71,15 @@ const Directory = ({
 		)
 	)
 
+	const [
+		directoryContents,
+		setDirectoryContents,
+	] = (
+		useState(
+			initialDirectoryContents
+		)
+	)
+
 	const goToDirectory = (
 		useCallback(
 			() => {
@@ -60,13 +94,90 @@ const Directory = ({
 		)
 	)
 
+	useEffect(
+		() => {
+			if (!directoryPath) {
+				return
+			}
+
+			const subscriber = (
+				bindNodeCallback(
+					fs
+					.readdir
+					.bind(fs)
+				)(
+					directoryPath,
+					{ withFileTypes: true },
+				)
+				.pipe(
+					mergeAll(),
+					map(directoryEntry => ({
+						fileName: (
+							directoryEntry
+							.name
+						),
+						filePath: (
+							path
+							.join(
+								directoryPath,
+								(
+									directoryEntry
+									.name
+								),
+							)
+						),
+						isDirectory: (
+							directoryEntry
+							.isDirectory()
+						),
+						isFile: (
+							directoryEntry
+							.isFile()
+						),
+					})),
+					toArray(),
+				)
+				.subscribe(
+					setDirectoryContents
+				)
+			)
+
+			return () => {
+				subscriber
+				.unsubscribe()
+			}
+		},
+		[directoryPath],
+	)
+
+	const imageFiles = (
+		useImageFiles(
+			directoryContents
+		)
+	)
+
 	return (
 		<div
 			css={directoryStyles}
 			onClick={goToDirectory}
 		>
-			<div css={textStyles}>
-				{directoryName}
+			<div css={directoryContentStyles}>
+				<div css={textStyles}>
+					{directoryName}
+				</div>
+
+				{
+					imageFiles[0]
+					&& (
+						<div css={imageStyles}>
+							<Image
+								fileName={imageFiles[0].name}
+								filePath={imageFiles[0].path}
+								hasVisibilityDetection
+							/>
+						</div>
+					)
+				}
 			</div>
 		</div>
 	)
