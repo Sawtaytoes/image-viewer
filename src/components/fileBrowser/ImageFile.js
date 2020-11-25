8b4d9d113978651796be6421fcc97b13f9eecb98
Kevin Ghadyani <Saturn2888@gmail.com>
2020-11-25 15:34:07 -0600

    Used width and height 100% in ImageFile instead of bottom: 0 and right: 0

--- src/components/fileBrowser/ImageFile.js
+++ src/components/fileBrowser/ImageFile.js
@@ -23,11 +23,11 @@ const imageFileStyles = css`
 `
 
 const imageStyles = css`
-	bottom: 0;
+	height: 100%;
 	left: 0;
 	position: absolute;
-	right: 0;
 	top: 0;
+	width: 100%;
 `
 
 const propTypes = {
