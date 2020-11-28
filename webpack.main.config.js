4f1dc95274cfa9d38407a868c0c1d1298310e997
Kevin Ghadyani <Saturn2888@gmail.com>
2020-11-28 03:25:44 -0600

    Removed unused rule in webpack.main.config.js

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,18 +1,5 @@
 const rules = require('./webpack.rules')
 
-rules
-.push({
-	enforce: 'pre',
-	exclude: /node_modules/,
-	test: /\.(js|jsx)$/,
-	use: {
-		loader: 'eslint-loader',
-		options: {
-			fix: true,
-		},
-	},
-})
-
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
