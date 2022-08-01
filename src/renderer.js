989e398e37fc2a7629ae65caa732b4a528621de9
Kevin Ghadyani <Saturn2888@gmail.com>
2022-08-01 06:44:21 -0500

    Down-sized zoom factor because the latest Surface Pro is scaled up
    Eventually, this scaling will need to be DPI-dependent.

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,12 @@
+import { webFrame } from 'electron'
 import { render } from 'react-dom'
 
 import ReactRoot from './components/ReactRoot'
+
+webFrame
+.setZoomFactor(
+	0.75
+)
 
 render(
 	<ReactRoot />,
